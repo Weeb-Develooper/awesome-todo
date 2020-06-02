68df1d6c168154d7912f34b0cbc14887fefc69e7
Rexsunon <osikhenaevidence@gmail.com>
2020-06-02 06:17:20 +0100

    Initial commit

---
+++ lib/models/Categories.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Categories {
+  final Color color;
+  final Icon icon;
+  final String name;
+  final int itemCount;
+
+  Categories(this.color, this.icon, this.name, this.itemCount);
+}
